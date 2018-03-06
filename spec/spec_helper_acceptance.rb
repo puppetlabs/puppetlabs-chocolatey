8a8ac62722b2619df17017c78ceb70d25a1b802a
Bill Hurt <hurt.bill@gmail.com>
2018-03-06 10:00:10 -0800

    Work on spec_helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,16 +6,15 @@ require 'nokogiri'
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
+require 'beaker/ca_cert_helper'
 
 run_puppet_install_helper
-
 install_ca_certs
 
-proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+PROJ_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
 hosts.each do |host|
   install_module_dependencies_on(host)
-  install_dev_puppet_module_on(host, :source => proj_root, :module_name => 'chocolatey')
 end
 
 def windows_agents
