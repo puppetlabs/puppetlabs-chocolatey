5dede7cae894e695936d93ccdaeb0ecf3581d265
skyamgarp <130442619+skyamgarp@users.noreply.github.com>
2025-07-21 22:25:34 +0530

    (MODULES-11578) Replace legacy facts

--- lib/facter/chocolateyversion.rb
+++ lib/facter/chocolateyversion.rb
@@ -4,7 +4,7 @@ require 'pathname'
 require Pathname.new(__FILE__).dirname + '../' + 'puppet_x/chocolatey/chocolatey_version'
 
 Facter.add('chocolateyversion') do
-  confine osfamily: :windows
+  confine 'os.family': :windows
   setcode do
     choco_ver = PuppetX::Chocolatey::ChocolateyVersion.version || '0'
     choco_ver.to_s
